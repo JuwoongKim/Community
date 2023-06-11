deb4e9a2bddbc7c8a081d343eb3d3dcafbc4f1ad
Juwoong <wwong7247@gmail.com>
2023-06-12 08:55:29 +0900

    feat : 데이터베이스 연동
    *  DataAcess 기술로  JPA를 사용 
    *  DataBase는 MySql을 사용
    
    todo :  도커에서  DB 이미지를 사용할 예정

---
+++ src/main/java/com/juwoong/community/entity/User.java
@@ -0,0 +1,30 @@
+package com.juwoong.community.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "USERS")
+public class User {
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String email;
+
+	@Column(nullable = false, length = 100)
+	private String password;
+
+	@Column(nullable = false, length = 100)
+	private String nickName;
+
+	@Column(nullable = false, length = 500)
+	private String stateMessage;
+
+}
